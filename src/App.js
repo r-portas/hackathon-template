f12b7b989ac52a191a443b4e641398985cc68824
Roy Portas <royportas@gmail.com>
2017-10-27 10:08:35 +1000

    Fixed path names

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div className="App">
             <Header/>
 
